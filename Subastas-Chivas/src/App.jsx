d9f39bda984b6d6c5126f0629e404bd11e3b690d
1NZUNZA <a01644973@tec.mx>
2025-05-28 10:20:06 -0600

    Dashboard usuario

--- Subastas-Chivas/src/App.jsx
+++ Subastas-Chivas/src/App.jsx
@@ -5,6 +5,9 @@ import Header from './Header';
 import Footer from './Footer';
 import LockerRoomSearch from './LockerRoomSearch';
 import LockerAuction from './LockerAuction';
+import CalendarSelector from './CalendarSelector';
+import DashboardUsuario from './DashboardUsuario';
+
 /*
 import LockerRoom from './LockerRoom';
 
@@ -20,6 +23,9 @@ const App = () => {
           <Route path="/" element={<DoorCarousel/>} />
           <Route path="/auction/:auctionID" element={<LockerAuction />} />
           <Route path="/locker-room-search" element={<LockerRoomSearch />} />
+          <Route path="/calendar" element={<CalendarSelector />} />
+          <Route path="/dashboard" element={<DashboardUsuario />} />
+          <Route path="/" element={<h1 style={{ color: 'white', textAlign: 'center', marginTop: '100px' }}>Bienvenido a Subastas Chivas</h1>} />
         </Routes>
         <Footer />
       </Router>
