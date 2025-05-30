ab0ec0a9d58acc9ec43accb01b4a1702d96bb960
1NZUNZA <a01644973@tec.mx>
2025-05-30 09:41:39 -0600

    Cambios en el dashboard de usuario y administrador

--- Subastas-Chivas/src/App.jsx
+++ Subastas-Chivas/src/App.jsx
@@ -29,6 +29,7 @@ const App = () => {
           <Route path="/locker-room-search" element={<LockerRoomSearch />} />
           <Route path="/calendar" element={<CalendarSelector />} />
           <Route path="/dashboardUsuario" element={<DashboardUsuario />} />
+          <Route path="/dashboardForm" element={<DashboardForm />} />
           <Route path="/" element={<h1 style={{ color: 'white', textAlign: 'center', marginTop: '100px' }}>Bienvenido a Subastas Chivas</h1>} />
           <Route
             path="/payment-setup"
